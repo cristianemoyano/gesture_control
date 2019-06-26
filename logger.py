e8744b7f757019f8dd1d1c86b8b04a57d260fdd3
eb-cmoyano <cmoyano@eventbrite.com>
2019-06-25 23:58:27 -0300

    initial commit

---
+++ logger.py
@@ -0,0 +1,15 @@
+import logging
+
+def get_logger():
+    # create logger
+    logger = logging.getLogger('arduino')
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    # create formatter
+    formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+    # add formatter to ch
+    ch.setFormatter(formatter)
+    # add ch to logger
+    logger.addHandler(ch)
+    return logger
---
+++ main.py
@@ -0,0 +1,127 @@
+import time
+
+import pyautogui
+from serial import Serial
+from serial.tools import list_ports
+
+from logger import get_logger
+
+
+logger = get_logger()
+
+SERIAL_DEVICE_NAME = '/dev/cu.usbmodem1411'
+
+PRESS_ACTION_TYPE = 'press'
+HOTKEY_ACTION_TYPE = 'hotkey'
+
+YOUTUBE_ACTIONS = {
+    'play_pause': {
+        'shortcut': ['space'],
+        'type': PRESS_ACTION_TYPE,
+    },
+    'rewind': {
+        'shortcut': ['left'],
+        'type': PRESS_ACTION_TYPE,
+    },
+    'forward': {
+        'shortcut': ['right'],
+        'type': PRESS_ACTION_TYPE,
+    },
+    'volume_up': {
+        'shortcut': ['shift','up'],
+        'type': HOTKEY_ACTION_TYPE,
+    },
+    'volume_down': {
+        'shortcut': ['shift','down'],
+        'type': HOTKEY_ACTION_TYPE,
+    },
+    'mute_unmute': {
+        'shortcut': ['m'],
+        'type': PRESS_ACTION_TYPE,
+    },
+    'fullscreen': {
+        'shortcut': ['f'],
+        'type': PRESS_ACTION_TYPE,
+    },
+    'escape': {
+        'shortcut': ['esc'],
+        'type': PRESS_ACTION_TYPE,
+    },
+    'captions': {
+        'shortcut': ['c'],
+        'type': PRESS_ACTION_TYPE,
+    },
+}
+
+def control(action):
+    action_selected = YOUTUBE_ACTIONS.get(str(action))
+    logger.info("action: {}".format(action))
+    if action_selected:
+        if action_selected['type'] == PRESS_ACTION_TYPE:
+            pyautogui.press(*action_selected['shortcut'])
+        elif action_selected['type'] == HOTKEY_ACTION_TYPE::
+            pyautogui.hotkey(*action_selected['shortcut'])
+    else:
+        logger.warning("Action not found.")
+
+def demo_controls():
+    for k,_ in YOUTUBE_ACTIONS.items():
+        time.sleep(2)
+        control(k)
+
+
+def read_serial(port, baudrate=9600):
+    """
+    Open a serial connection.
+
+    Doc: https://pyreadthedocs.io/en/latest/pyserial_api.html
+
+    Args:
+    - port: (string|required) Device name
+    - baudrate: (int|required) Baud rate such as 9600 or 115200 etc.
+
+    """
+    # Setup
+    arduino_comm = Serial()
+    arduino_comm.port = port
+    arduino_comm.baudrate = baudrate
+
+    try:
+        arduino_comm.open()
+        logger.info("Serial port opened: {port} - Baud rate: {baudrate} bits/second".format(
+            port=port,
+            baudrate=baudrate,
+        ))
+    except Exception as e:
+        logger.error("Error open serial port: {}".format(e))
+
+    if arduino_comm.isOpen():
+        try:
+            while True:
+                message = arduino_comm.readline().decode("utf-8").rstrip()
+                logger.info("Msg received: {}".format(message))
+                control(action=message)
+        except Exception as e1:
+            logger.error("Error communicating: {}".format(e1))
+        except KeyboardInterrupt:
+            logger.info("Exit.")
+        finally:
+            arduino_comm.close()
+            logger.info("Device closed")
+    else:
+        logger.error("Cannot open serial port")
+
+
+def serial_ports():
+    """ Lists serial port names
+
+    returns:
+        A list of the serial ports available on the system
+    """
+    ports = list(list_ports.comports())
+    for port in ports:
+        logger.info(port.__dict__)
+
+
+if __name__ == "__main__":
+    read_serial(SERIAL_DEVICE_NAME)
